06b6f54286abdefd496942be25832689c2184ada
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-05-24 22:34:14 -0500

    feat: :sparkles: se reolvio la hoja # 4 del taller de POO
    Primero se creo la clase estudiante extendiendola de Persona, agregando las propiedades y los metodos indicados en el PDF

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 // Llamo la clase saludarA del Documento
+const bocadillo = document.querySelector('.bocadillo-cuadrado')
 const saludar = document.querySelector('.saludarA')
+const dedicas = document.querySelector('.Q1')
 
 //* Creo la clase Persona
 class Persona{
@@ -14,14 +16,36 @@ class Persona{
     }
 
     saludar(){
-        let hola = `Hola, me llamo ${this.nombre} y me gusta comer `//? Cree esto de esta forma pues me dio pereza y se veia feo el poner esto directamente abajo
-        document.querySelector('.bocadillo-cuadrado').innerHTML = hola 
+        return `Hola, me llamo ${this.nombre} y me gusta comer `//? Cree esto de esta forma pues me dio pereza y se veia feo el poner esto directamente abajo
     }
 }
+
 //* Creo nueva persona con caracteristicas por default
 const persona1 = new Persona({nombre: 'Carlos', edad:18,sexo:"hombre"})
 
-//! Esto no es necesario pero yo lo puse para dar utilidad a la constante saludar 
+//* Crea Estudiante que se extiende de Persona es decir llamo a Estudiante o instacio a estudiante como clase "hija" de Persona para que herede sus atributos
+class Estudiante extends Persona{
+    constructor({ nombre = "Carlos", edad = 15, sexo = "M", carrera = "programación" }) {
+        super({nombre,edad,sexo}) // <= Aca estoy llamando lo que quiero heredar de la clase padre
+        this.carrera=carrera // <= Esto no es más que "creacion"/"instaciacion" de un nuevo atributo
+    }
+
+    //* metodo que retorna texto
+    estudiar(){
+        return(`Estoy estudiando ${this.carrera}`);
+    }
+}
+
+let estudiante1= new Estudiante({nombre:"Oscar M Alvarez Gómez",edad:18,sexo:"M",carrera:"Ing Electrica"})
+
+//console.log(Estudiante1.estudiar());
+
+
+//! Estos son los eventos y los cambios en el documento
 saludar.addEventListener('click', ()=>{
-    persona1.saludar()
+    bocadillo.innerHTML = persona1.saludar()
+})
+
+dedicas.addEventListener('click',()=>{
+    bocadillo.innerHTML = estudiante1.estudiar()
 })
